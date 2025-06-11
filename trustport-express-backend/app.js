681ab4f53ff4667d900acf780a07257a1929b428
nazzyrichie13 <youngnazzy13@gmail.com>
2025-06-11 14:21:16 +0100

    Fix MongoDB connection warning and update contact.html

--- trustport-express-backend/app.js
+++ trustport-express-backend/app.js
@@ -20,12 +20,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB error:', err));
+mongoose.connect(process.env.MONGODB_URI);
+
+
 
 // Routes
 app.use('/api/shipments', require('./routes/shipments'));
